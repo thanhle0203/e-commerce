09ed54d29524ec70a87b28dbeae0d66c21c5bbf1
Thanh Le <thanhlevan0203@gmail.com>
2022-11-04 23:25:05 -0700

    Create Products Page

--- e-commerce/pages/index.tsx
+++ e-commerce/pages/index.tsx
@@ -4,8 +4,17 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
-      Test
+    <div className='p-5'>
+      <div>
+        <h2>Mobiles</h2>
+        <div className='py-4'>
+          <div className='w-64'>
+            <div className='bg-blue-100 p-5 rounded-xl'>
+              <img src='/products/iphone.png' alt='' />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

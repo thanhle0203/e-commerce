6f605c3ccaa7d0cebdb139763702978495f9c89b
Thanh Le <thanhlevan0203@gmail.com>
2022-11-05 14:49:08 -0700

    Create Search Bar Component

--- e-commerce/pages/index.tsx
+++ e-commerce/pages/index.tsx
@@ -6,7 +6,7 @@ import styles from '../styles/Home.module.css'
 import Product from "../component/Product"
 
 export default function Home() {
-  const [productsInfo, setProductsInfo] = useState('');
+  const [productsInfo, setProductsInfo] = useState([]);
   useEffect(() => {
     fetch('api/products')
       .then(response => response.json())
@@ -18,13 +18,18 @@ export default function Home() {
 
   return (
     <div className='p-5'>
+      <input type="text" placeholder="Search for products..." className="bg-gray-100 w-full py-2 px-4 rounded-xl" />
       <div>
         {categoriesNames.map(categoryName => (
           <div key={categoryName}>
-            <h2 className='text-2xl capitalize'>{categoryName}</h2>
-            {productsInfo.filter(p => p.category === categoryName).map(productInfo => (
-              <Product {...productInfo} />
-            ))}
+            <h2 className='text-2xl py-5 capitalize'>{categoryName}</h2>
+            <div className='flex -mx-5 overflow-x-scroll snap-x scrollbar-hide'>
+              {productsInfo.filter(p => p.category === categoryName).map(productInfo => (
+                <div key={productInfo._id} className="px-5 snap-start">
+                  <Product {...productInfo} />
+                </div>
+              ))}
+            </div>
           </div>
         ))}
 

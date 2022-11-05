42c38641e94d1f222ef77c3b012d34d2534af245
Thanh Le <thanhlevan0203@gmail.com>
2022-11-05 14:16:14 -0700

    Create Product component

--- e-commerce/pages/index.tsx
+++ e-commerce/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { useEffect, useState } from 'react';
 import styles from '../styles/Home.module.css'
+import Product from "../component/Product"
 
 export default function Home() {
   const [productsInfo, setProductsInfo] = useState('');
@@ -21,27 +22,15 @@ export default function Home() {
         {categoriesNames.map(categoryName => (
           <div key={categoryName}>
             <h2 className='text-2xl capitalize'>{categoryName}</h2>
-            {productsInfo.filter(p => p.category === categoryName).map(product => (
-              <div>{product.name}</div>
+            {productsInfo.filter(p => p.category === categoryName).map(productInfo => (
+              <Product {...productInfo} />
             ))}
           </div>
         ))}
 
         <h2 className='text-2xl'>Mobiles</h2>
         <div className='py-4'>
-          <div className='w-64'>
-            <div className='bg-blue-100 p-5 rounded-xl'>
-              <img src='/products/iphone.png' alt='' />
-            </div>
-            <div className='mt-2'>
-              <h3 className='font-bold text-lg'>Iphone 14 Pro</h3>
-            </div>
-            <p className='text-sm mt-1 leading-4'>Lorem ipsum dolor sit amet, ex sit vitae viderer tacimates. Duo ocurreret euripidis mnesarchum ad, sed inani solet temporibus ea. Debitis senserit et qui. </p>
-            <div className='flex mt-1'>
-              <div className='text-2xl font-bold grow'>$999</div>
-              <button className='bg-black text-white py-1 px-3 rounded'>+</button>
-            </div>
-          </div>
+          
         </div>
       </div>
     </div>

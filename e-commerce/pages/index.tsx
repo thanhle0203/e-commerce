5fd97d181b3146523db2cbd01bd98829b25a4bce
Thanh Le <thanhlevan0203@gmail.com>
2022-11-05 15:01:04 -0700

    Fetching products with getServerSideProps

--- e-commerce/pages/index.tsx
+++ e-commerce/pages/index.tsx
@@ -7,6 +7,8 @@ import Product from "../component/Product"
 
 export default function Home() {
   const [productsInfo, setProductsInfo] = useState([]);
+  const [phrase,setPhrase] = useState('');
+
   useEffect(() => {
     fetch('api/products')
       .then(response => response.json())
@@ -15,28 +17,36 @@ export default function Home() {
 
   const categoriesNames = [...new Set(productsInfo.map(p => p.category))];
   
+  let products;
+  if (phrase) {
+    products = productsInfo.filter(p => p.name.toLowerCase().includes(phrase));
+  } else {
+    products = productsInfo;
+  }
 
   return (
     <div className='p-5'>
-      <input type="text" placeholder="Search for products..." className="bg-gray-100 w-full py-2 px-4 rounded-xl" />
+
+      <input value={phrase} onChange={e => setPhrase(e.target.value)} type="text" placeholder="Search for products..." className="bg-gray-100 w-full py-2 px-4 rounded-xl" />
       <div>
         {categoriesNames.map(categoryName => (
           <div key={categoryName}>
-            <h2 className='text-2xl py-5 capitalize'>{categoryName}</h2>
-            <div className='flex -mx-5 overflow-x-scroll snap-x scrollbar-hide'>
-              {productsInfo.filter(p => p.category === categoryName).map(productInfo => (
-                <div key={productInfo._id} className="px-5 snap-start">
-                  <Product {...productInfo} />
+            {products.find(p => p.category === categoryName) && (
+              <div>
+                <h2 className='text-2xl py-5 capitalize'>{categoryName}</h2>
+                <div className='flex -mx-5 overflow-x-scroll snap-x scrollbar-hide'>
+                  {products.filter(p => p.category === categoryName).map(productInfo => (
+                    <div key={productInfo._id} className="px-5 snap-start">
+                      <Product {...productInfo} />
+                    </div>
+                  ))}
                 </div>
-              ))}
-            </div>
+              </div>
+            )}
+            
           </div>
         ))}
 
-        <h2 className='text-2xl'>Mobiles</h2>
-        <div className='py-4'>
-          
-        </div>
       </div>
     </div>
   )

97dc9274f9831573032320010d513398bdf95591
Thanh Le <thanhlevan0203@gmail.com>
2022-11-05 14:06:20 -0700

    Fetch Product DB

--- e-commerce/pages/index.tsx
+++ e-commerce/pages/index.tsx
@@ -1,11 +1,32 @@
+
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect, useState } from 'react';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [productsInfo, setProductsInfo] = useState('');
+  useEffect(() => {
+    fetch('api/products')
+      .then(response => response.json())
+      .then(json => setProductsInfo(json))
+  }, []);
+
+  const categoriesNames = [...new Set(productsInfo.map(p => p.category))];
+  
+
   return (
     <div className='p-5'>
       <div>
+        {categoriesNames.map(categoryName => (
+          <div key={categoryName}>
+            <h2 className='text-2xl capitalize'>{categoryName}</h2>
+            {productsInfo.filter(p => p.category === categoryName).map(product => (
+              <div>{product.name}</div>
+            ))}
+          </div>
+        ))}
+
         <h2 className='text-2xl'>Mobiles</h2>
         <div className='py-4'>
           <div className='w-64'>

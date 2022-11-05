c5fca633201746d33c997e4ea183c9236c91bea4
Thanh Le <thanhlevan0203@gmail.com>
2022-11-05 13:32:09 -0700

    Fetch mongoDB database

--- e-commerce/pages/api/products.js
+++ e-commerce/pages/api/products.js
@@ -1,3 +1,8 @@
+import { initMongoose } from "../../lib/mongoose";
+import Product from "../../models/Product";
+
 export default async function handle(req, res) {
-    
+  await initMongoose();
+
+  res.json(await Product.find().exec());
 }

793dfdc1832a423414405ac606044baa0c5a4f80
Thanh Le <thanhlevan0203@gmail.com>
2022-11-04 23:48:15 -0700

    Create Product Card

--- e-commerce/pages/index.tsx
+++ e-commerce/pages/index.tsx
@@ -12,6 +12,14 @@ export default function Home() {
             <div className='bg-blue-100 p-5 rounded-xl'>
               <img src='/products/iphone.png' alt='' />
             </div>
+            <div className='mt-2'>
+              <h3 className='font-bold text-lg'>Iphone 14 Pro</h3>
+            </div>
+            <p className='text-sm mt-1 leading-4'>Lorem ipsum dolor sit amet, ex sit vitae viderer tacimates. Duo ocurreret euripidis mnesarchum ad, sed inani solet temporibus ea. Debitis senserit et qui. </p>
+            <div className='flex mt-1'>
+              <div className='text-2xl font-bold grow'>$999</div>
+              <button className='bg-black text-white py-1 px-3 rounded'>+</button>
+            </div>
           </div>
         </div>
       </div>

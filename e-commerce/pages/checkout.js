34e276fd30d7dcbca930c326ada35acd3b785c67
Thanh Le <thanhlevan0203@gmail.com>
2022-11-11 11:11:56 -0800

    Create checkout page component

--- e-commerce/pages/checkout.js
+++ e-commerce/pages/checkout.js
@@ -1,9 +1,18 @@
 import Layout from '../component/Layout'
 
 export default function CheckoutPage() {
+  const {selectedProducts} = useContext(ProductsContext);
+  const [productsInfo,setProductsInfo] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/products?ids='+selectedProducts.join(','))
+      .then(response => response.json())
+      .then(json => setProductsInfo(json));
+  }, []);
+  
   return (
     <Layout>
-        checkout
+      {selectedProducts.join(',')}
     </Layout>
   );
 }
